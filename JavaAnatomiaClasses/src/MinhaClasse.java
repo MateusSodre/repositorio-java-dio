e3459fc5b197f4885221be7e404cda4083a18334
Sodre <mateus.sodre.s@gmail.com>
2023-01-23 13:41:36 -0300

    aula anatomia das classes3

--- JavaAnatomiaClasses/src/MinhaClasse.java
+++ JavaAnatomiaClasses/src/MinhaClasse.java
@@ -1,8 +1,20 @@
 public class MinhaClasse {
 
     public static void main (String [] args) {
-        System.out.print("Hello World");
+      // teste aula anatomia das classes -3
+        String primeiroNome = "Mateus";
+        String segundoNome = "Sodre";
+
+        String  nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+
         }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat("").concat(segundoNome);
+    }
+
 
     
 }
