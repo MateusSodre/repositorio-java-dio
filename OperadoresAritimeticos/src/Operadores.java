59e45f2edd23f474086e0f607f72c1714a81bf3c
Sodre <mateus.sodre.s@gmail.com>
2023-01-24 17:29:11 -0300

    concatenação e interações matematicas

--- OperadoresAritimeticos/src/Operadores.java
+++ OperadoresAritimeticos/src/Operadores.java
@@ -1,5 +1,26 @@
 public class Operadores {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        String concatenacao ="?"; 
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao
+        );
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
     }
 }
